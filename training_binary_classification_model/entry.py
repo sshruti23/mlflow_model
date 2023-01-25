56dba025e65cdbc08c73b3eb1c5fb7d8dc47fa8f
shrutis23 <sshruti2310@gmail.com>
2023-01-25 15:46:03 +0530

    Added package resource to fetch file

--- training_binary_classification_model/entry.py
+++ training_binary_classification_model/entry.py
@@ -1,5 +1,5 @@
 import argparse
-import importlib
+from pkg_resources import resource_string
 
 parser = argparse.ArgumentParser(
     prog="Library Code", description="Main Entry", epilog="Text at the bottom of help"
@@ -9,7 +9,17 @@ parser.add_argument("-m", "--module")
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    print(args.module)
+    print("Module :" + args.module)
 
-    model_module = importlib.import_module(args.module)
+    import os
+    from os.path import isfile, join
+
+    print("--------")
+    cwd = os.getcwd()
+    onlyfiles = [os.path.join(cwd, f) for f in os.listdir(cwd) if
+                 os.path.isfile(os.path.join(cwd, f))]
+    print(onlyfiles)
+    print("--------")
+
+    model_module = resource_string(__name__, args.module)
     model_module.start()
