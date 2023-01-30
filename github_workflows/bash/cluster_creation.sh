85487b27a5f8c434f17f6873ae6bd66920d106fb
shrutis23 <sshruti2310@gmail.com>
2023-01-30 16:58:11 +0530

    fixed output redirection error

--- github_workflows/bash/cluster_creation.sh
+++ github_workflows/bash/cluster_creation.sh
@@ -5,5 +5,4 @@ output=$(curl -X POST https://$host/api/2.0/clusters/create \
               -H "Authorization:Bearer $pat_token" \
               -H "Content-Type: application/json" \
               --data @github_workflows/data/cluster_creation_request.json)
-id=$( jq -r  '.cluster_id' <<< "${output}" )
-echo "cluster_id=$id" >> $GITHUB_ENV
+echo "$( jq -r  '.cluster_id' <<< "${output}" )"
