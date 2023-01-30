613c60a84003454325de15655f5a2b768520ccb2
shrutis23 <sshruti2310@gmail.com>
2023-01-30 22:02:18 +0530

    Create simple folder structure for scripts

--- github_workflows/bash/cluster_creation.sh
+++ github_workflows/bash/cluster_creation.sh
@@ -1,10 +1,1 @@
 #!/bin/bash
-host=$1
-pat_token=$2
-
-output=$(curl -X POST https://$host/api/2.0/clusters/create \
-              -H "Authorization:Bearer $pat_token" \
-              -H "Content-Type: application/json" \
-              --data @github_workflows/data/cluster_creation_request.json)
-id=$( jq -r  '.cluster_id' <<< "${output}" )
-echo "$( jq -r  '.cluster_id' <<< "${id}" )"
