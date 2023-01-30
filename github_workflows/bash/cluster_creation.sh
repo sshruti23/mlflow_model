e1fff1da279c5281d7e58cc3e8f3d11145f26b3c
shrutis23 <sshruti2310@gmail.com>
2023-01-30 17:12:48 +0530

    Updated permissions to run on github workflows

--- github_workflows/bash/cluster_creation.sh
+++ github_workflows/bash/cluster_creation.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 host=$1
 pat_token=$2
+
 output=$(curl -X POST https://$host/api/2.0/clusters/create \
               -H "Authorization:Bearer $pat_token" \
               -H "Content-Type: application/json" \
               --data @github_workflows/data/cluster_creation_request.json)
+
 echo "$( jq -r  '.cluster_id' <<< "${output}" )"
--- github_workflows/bash/training_job.sh
+++ github_workflows/bash/training_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 host=$1
 pat_token=$2
+
 curl -X POST https://$host/api/2.1/jobs/runs/submit \
 -H "Authorization:Bearer $pat_token}" \
 -H "Content-Type: application/json" \
