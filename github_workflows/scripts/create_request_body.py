cfa15ccc87c8d40d5d3c1b785bc736259bec95d4
S Shruti <51433931+sshruti23@users.noreply.github.com>
2023-01-30 21:01:01 +0530

    Workflows using databricks repo and one cluster for all tasks (#5)
    * changed bash run syntax
    
    * Updated permissions to run on github workflows
    
    * run scripts with sh syntax
    
    * Update workflows
    
    * Create a proper json to be used as request body
    
    * Commented few steps for faster testing
    
    * Switch to args 0
    
    * Add jq to capture cluster id from response
    
    * arg 1 to be used , arg 0 is the file name
    
    * clean up code
    
    * remove the wrongly added file delete command

--- github_workflows/scripts/create_request_body.py
+++ github_workflows/scripts/create_request_body.py
@@ -8,10 +8,10 @@ def get(databricks_cluster_id: str):
     file = open("github_workflows/data/training_single_cluster.json")
     data = json.load(file)
     template = Template(str(data))
+
     request_body = template.render(cluster_id=databricks_cluster_id)
-    # print(json.dumps(request_body))
     with open("github_workflows/data/request.json", "w+") as outfile:
-        outfile.write(json.dumps(request_body, indent=4))
+        outfile.write(request_body.replace("\'", "\""))
 
 
 if __name__ == '__main__':
