4150890c135d9d92868a0307737c1687c3800cb5
shrutis23 <sshruti2310@gmail.com>
2023-01-30 16:33:14 +0530

    fixed bash file params

--- github_workflows/bash/cluster_creation.sh
+++ github_workflows/bash/cluster_creation.sh
@@ -1,5 +1,8 @@
-output=$(curl -X POST https://${{ secrets.AWS_DB_HOST }}/api/2.0/clusters/create \
-              -H "Authorization:Bearer ${{ secrets.AWS_DB_TOKEN }}" \
+#!/bin/bash
+host=$1
+pat_token=$2
+output=$(curl -X POST https://$host/api/2.0/clusters/create \
+              -H "Authorization:Bearer $pat_token" \
               -H "Content-Type: application/json" \
               --data @github_workflows/data/cluster_creation_request.json)
 id=$( jq -r  '.cluster_id' <<< "${output}" )
--- github_workflows/bash/training_job.sh
+++ github_workflows/bash/training_job.sh
@@ -1,5 +1,8 @@
-curl -X POST https://${{ secrets.AWS_DB_HOST }}/api/2.1/jobs/runs/submit \
--H "Authorization:Bearer ${{ secrets.AWS_DB_TOKEN }}" \
+#!/bin/bash
+host=$1
+pat_token=$2
+curl -X POST https://$host/api/2.1/jobs/runs/submit \
+-H "Authorization:Bearer $pat_token}" \
 -H "Content-Type: application/json" \
 --data @github_workflows/data/training_single_cluster.json
 
