1e6d6eade0f08e8dc59e2c738d169122e8c8e23e
shrutis23 <sshruti2310@gmail.com>
2023-02-07 19:05:05 +0530

    Demostration databricks workflows using python scripts , db jobs and db repos

---
+++ stockpred/data_preparation.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def digitize(n):
+    if n > 0:
+        return 1
+    return 0
+
+
+def acquire_training_data():
+    df = pd.read_csv('/dbfs/request_body/raw.csv')
+    print("acquire_training_data")
+    return df
+
+
+def prepare_training_data(data):
+    """
+    Return a prepared numpy dataframe
+    input : Dataframe with expected schema
+
+    """
+    print("prepare_training_data")
+    data["Delta"] = data["Close"] - data["Open"]
+    data["to_predict"] = data["Delta"].apply(lambda d: digitize(d))
+    return data
+
+
+def prepare_data(X, Y):
+    print("prepare_data")
+    X = pd.DataFrame(X)
+    X.columns = ["day_" + str(i) for i in range(14)]
+    Y = pd.DataFrame(Y)
+    Y.columns = ["to_predict"]
+    df = pd.concat([X, Y], axis=1)
+
+    train_data, test_data = train_test_split(df, test_size=0.25, random_state=4284)
+    return train_data, test_data
