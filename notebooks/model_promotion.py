3e34d00cd1b017d51a25bc285b19eabbc93ecd60
sshruti23 <sshruti23>
2023-02-15 07:46:45 +0000

    Added checks to avoid task failure during model promotion

--- notebooks/model_promotion.py
+++ notebooks/model_promotion.py
@@ -5,7 +5,10 @@ import mlflow.sklearn
 # COMMAND ----------
 
 version= dbutils.jobs.taskValues.get(taskKey = "Evaluate", key = "best_model_version", default = None, debugValue = 0)
-print(f"best model version {version}")
+best_model_version=int(version)
+print(best_model_version)
+print(type(best_model_version))
+print(f"best model version {best_model_version}")
 
 
 # COMMAND ----------
@@ -20,11 +23,17 @@ print("promote best model version to Staging")
 
 registered_stages=client.get_latest_versions("stockpred_model",["Staging"])
 registered_version=int(registered_stages[0].version)
-if registered_version is not version:
-    client.transition_model_version_stage(
-        name="stockpred_model", version=version, stage="Staging"
-    )
-    print(f"version {version} moved to STAGING")
 
-print("Model is already registered as Staging.")
-print("Model Promotion Completed!!")
+print(f"best model version {best_model_version}")
+print(f"registered model version {registered_version}")
+
+
+if registered_version == best_model_version:
+    print("Model is already registered as Staging.")
+    print("Model Promotion Completed!!")
+    
+else:
+    client.transition_model_version_stage(
+        name="stockpred_model", version=best_model_version, stage="Staging"
+    )
+    print(f"version {best_model_version} moved to STAGING")
